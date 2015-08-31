ea67a11e5d367300d612d9f03daf0cb54cad49fe
Felix Gluschenkov <felix.glusch@gmail.com>
2015-08-31 14:16:28 -0400

    Card view and settings page

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,17 @@
-/**
- * Created by Felix on 2015-08-23.
- */
+$(function(){
+    $('#colorPicker').colorpicker({
+        customClass: 'colorpicker-2x',
+        sliders: {
+            saturation: {
+                maxLeft: 200,
+                maxTop: 200
+            },
+            hue: {
+                maxTop: 200
+            },
+            alpha: {
+                maxTop: 200
+            }
+        }
+    });
+});
